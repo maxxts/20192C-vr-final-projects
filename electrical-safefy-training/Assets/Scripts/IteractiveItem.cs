6dc872ce6b777f992c23c6016d50835aa97432e7
Max <maxxts@gmail.com>
2019-11-20 21:07:09 -0300

    Initial Commit
    Initial commit with basic VR controller and interaction using GoogleVR SDK

---
+++ electrical-safefy-training/Assets/Scripts/IteractiveItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IteractiveItem : MonoBehaviour
+{
+    public void OnPointerExit()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+    public void OnPointerEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.yellow;
+    }
+
+    public void OnPointerClick()
+    {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+}
