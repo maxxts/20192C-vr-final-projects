4038fbeec93f3545fbfb732dc53d3ce00bca17c4
Max <maxxts@gmail.com>
2019-11-27 20:30:39 -0300

    Grab and Drop implemented
    Se agrega funcionalidad de Grab and Drop a los objectos que cuenten con tag "InteractiveItem" y componente "InteractiveItem". Se incluye objecto con mesh a modo de ejemplo, pero se deberian poder usar como padres de cualquier modelo que pueda ser interactivo.

--- electrical-safefy-training/Assets/Scripts/IteractiveItem.cs
+++ electrical-safefy-training/Assets/Scripts/IteractiveItem.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class IteractiveItem : MonoBehaviour
 {
-    public void OnPointerExit()
-    {
-        GetComponent<Renderer>().material.color = Color.green;
-    }
-
-    public void OnPointerEnter()
+    public void HighlightOn()
     {
         GetComponent<Renderer>().material.color = Color.yellow;
     }
 
-    public void OnPointerClick()
+    public void HighlightOff()
+    {
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+
+
+    public void Interact()
     {
         GetComponent<Renderer>().material.color = Color.red;
     }
